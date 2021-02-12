99a169ea6657239607c7bef76d466a4914334aa8
pray <comppro54@gmail.com>
2021-02-12 10:12:34 +0545

    Gauss elimmination sucessgit status!

--- gausseli.c
+++ gausseli.c
@@ -24,13 +24,13 @@ int main(){
 		}
 		printf("\n");
 	}
-	float x=0;
+	float x=0,y=0;
 	for (int i =0;i<r;i++){
 		x=A[i][i];
-		for (int j=i;j<=r;j++){
-			for(int k=i+1;k<r;k++){
-				A[k][j]=A[k][j]-(A[i][j]*A[k][i])/x;
-				
+		for (int j=i+1;j<r;j++){
+			y=A[j][i];
+			for(int k=i;k<=r;k++){
+				A[j][k]=A[j][k]-A[i][k]*(y/x);
 			}
 		}
 	}
@@ -40,6 +40,20 @@ int main(){
 			printf("%f\t",A[i][j]);
 		}
 			printf("\n");
+	}
+	float a[20];
+	a[r-1]=A[r-1][r]/A[r-1][r-1];
+//	printf("%f",a[r-1]);
+	for (int i=r-2;i>=0;i--){
+		a[i]=0;
+		for(int j=r-1;j>i;j--){
+			a[i]=a[i]+A[i][j]*a[j];
+		}
+		a[i]=(A[i][r]-a[i])/A[i][i];
+	}
+	for(int i=0;i<r;i++){
+		printf("%f\n",a[i]);
+
 	}
 	return 0;
 }
